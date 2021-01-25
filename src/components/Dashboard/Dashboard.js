0e4d3fd7d567ce41b0839d070ecbd1cba1ca7ce9
rhammock1 <rhamm1027@gmail.com>
2021-01-25 15:43:30 -0500

    Added dashboard comp

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = function() {
+  return (
+    <div className='dashboard'>
+      {/* Add language as header and a start practicing button. Later make an api request to get the words from the list and list them out in their component. */}
+    </div>
+  )
+}
+
+export default Dashboard;
--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Dashboard from '../../components/Dashboard/Dashboard'
 
 class DashboardRoute extends Component {
   render() {
     return (
       <section>
-        implement and style me
+        <h2>Dashboard</h2>
+        <Dashboard />
       </section>
     );
   }
