8aa0a88c76bf86d5f2a9656e00245fe699b51348
rhammock1 <rhamm1027@gmail.com>
2021-01-26 16:02:21 -0500

    Added total score and top 3 words to practice

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,18 +8,30 @@ class Dashboard extends React.Component {
   static contextType = WordContext;
 
   render() {
-    const { language } = this.context;
+    const { words } = this.context;
+    let total = 0;
+    words.map((word) => total += word.correct_count);
 
     return (
     <div className='dashboard'>
       {/* Add language as header and a start practicing button. Later make an api request to get the words from the list and list them out in their component. */}
       <div className='practice-container'>
-        <h3>My language: {language.name}</h3>
+        <h3>Total correct answers: {total}</h3>
         <button type="button">Start Practicing</button>
       </div>
       <div className='word-container'>
-          <p>Words to practice: </p>
-        {/* Gonna try and add a table instead of just doing a ul */}
+        
+        {(words.length)
+          ? <div className='practice'>
+              <h4>Words to practice: </h4>
+              <ul>
+                <li>{words[0].original}</li>
+                <li>{words[1].original}</li>
+                <li>{words[2].original}</li>
+              </ul>
+            </div>
+          : null
+        }
         <Table />
       </div>
     </div>
--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import Dashboard from '../../components/Dashboard/Dashboard'
+import WordContext from '../../contexts/WordContext';
 
 class DashboardRoute extends Component {
+  static contextType = WordContext;
+
   render() {
+    const { language } = this.context;
+    
     return (
       <section>
-        <h2>Dashboard</h2>
+        <h2>My language: {language.name}</h2>
         <Dashboard />
       </section>
     );
