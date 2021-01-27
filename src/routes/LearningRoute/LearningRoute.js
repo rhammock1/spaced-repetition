f653333be2a8bd02c87246947fddd57bcfdb2e9b
rhammock1 <rhamm1027@gmail.com>
2021-01-27 13:27:21 -0500

    setting up learning

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -5,11 +5,30 @@ class LearningRoute extends Component {
   
   static contextType = WordContext;
 
-  render() {
+  state = {
+    seen: false,
+    isCorrect: null,
+    error: null,
+  }
 
+  handleSeen = () => {
+    this.setState({ seen: true });
+  }
+
+  render() {
+    const { seen } = this.state;
     return (
       <section>
         <h2>Learning</h2>
+        {(!seen)
+          ? <div className='popup'>
+              <p>Are you ready?</p>
+              <button onClick={this.handleSeen} type='button'>Start</button>
+            </div>
+          : null 
+        }
+        {/* Learning card component */}
+        {/* AnswerResponse component */}
       </section>
     );
   }
