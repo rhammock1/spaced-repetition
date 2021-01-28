e0a4a67feb3017f985040b71a4932893e58a6530
rhammock1 <rhamm1027@gmail.com>
2021-01-28 12:44:40 -0500

    cypress 3.2 all passing

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -8,34 +8,18 @@ const Answer = function(props) {
     total,
     handleCloseAnswer,
     answer } = props;
+    let message = (isCorrect) ? 'You were correct! :D' : 'Good try, but not quite right :('
   return (
-    (isCorrect)
-      ? (
-        <div className='answer-container'>
-          <div className='DisplayFeedback'>
-            <h4>You were correct! :D</h4>
-          </div>
-          <div className='DisplayScore'>
-            <p>Your total score is: {total}</p>
-          </div>
-          <button onClick={handleCloseAnswer} type='button'>Try another</button>
-        </div>
-        
-      )
-      : (
-        <div className='answer-container'>
-          <div className='DisplayFeedback'>
-            <h2>Good try, but not quite right :(</h2>
-            <p>The correct translation for {word} was {answer} and you chose {guess}!</p>
-          </div>
-          <div className='DisplayScore'>            
-            <p>Your total score is: {total}</p>
-          </div>
-          <button onClick={handleCloseAnswer} type='button'>Try another word!</button>
-        
-        </div>
-      )
-    
+    <div className='answer-container'>
+      <div className='DisplayFeedback'>
+        <h2>{message}</h2>
+        <p>The correct translation for {word} was {answer} and you chose {guess}!</p>
+      </div>
+      <div className='DisplayScore'>
+        <p>Your total score is: {total}</p>
+      </div>
+      <button onClick={handleCloseAnswer} type='button'>Try another word!</button>
+    </div>
     )
 }
 
