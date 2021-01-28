3672636e0b447c1298b8bfc97821a1b6cec544ea
rhammock1 <rhamm1027@gmail.com>
2021-01-28 13:10:02 -0500

    edit test because total score was wrong

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -20,13 +20,13 @@ class Dashboard extends React.Component {
     <div className='dashboard'>
       {/* Add language as header and a start practicing button. Later make an api request to get the words from the list and list them out in their component. */}
       <div className='practice-container'>
-        <button type="button"><Link to='/learn'>Start Practicing</Link></button>
+        <button type="button"><Link to='/learn'>Start practicing</Link></button>
       </div>
       <div className='word-container'>
         
         {(words.length)
           ? <div className='practice'>
-              <h4>Words to practice: </h4>
+              <h3>Words to practice: </h3>
               <ul>
                 <li>{result[0].original}</li>
                 <li>{result[1].original}</li>
